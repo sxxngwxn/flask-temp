f4f493f270d41416eee64ebd330b4ed0b4fd58ab
sxxngwxn <sxxngw0n@gmail.com>
2025-08-19 14:43:16 +0900

    another page test

--- app.py
+++ app.py
@@ -5,3 +5,7 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return render_template('test/test.html')
+
+@app.route("/main")
+def main():
+    return render_template('service/login.html')
